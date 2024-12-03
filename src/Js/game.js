27afe05126c0a18ff586d802feee0b78d51da0db
Axel Ezn <axeleouzan@gmail.com>
2024-12-03 10:18:54 +0100

    MAJ

---
+++ src/Js/game.js
@@ -0,0 +1,15 @@
+//alert("Script Initialisé")
+class GameScene extends Phaser.Scene{
+    preload() {
+        this.load.image("tile", "assets/tile")
+    }
+}
+let config = {
+    width:800,
+    height:500,
+    backgroundColor : '#4488AA',
+    type:Phaser.AUTO,
+}
+const game = new Phaser.Game(config);
+game.scene.add("game", GameScene);
+game.scene.start("game");
