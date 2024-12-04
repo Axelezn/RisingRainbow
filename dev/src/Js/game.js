c8fef29b60e8df72c9c7566ac979530a2e394c14
Axel Ezn <axeleouzan@gmail.com>
2024-12-04 15:59:01 +0100

    Commit maj terrain

--- dev/src/Js/game.js
+++ dev/src/Js/game.js
@@ -1,10 +1,36 @@
 //alert("Script Initialisé")
 class GameScene extends Phaser.Scene{
+    //On load les assets ici
     preload() {
-        this.load.image("tile", "assets/tile.png")
+        this.load.image("tile", "assets/tile.png");
+        this.load.atlas("dino", "assets/rondoudou@2x.png")
     }
+    // Créer l'entité de jeu 
     create() {
-      this.add.image(200,300, "tile")  
+        this.tileWidth=64;
+        this.tileHeight = 64;
+        this.dino = this.add.sprite(200,343, "dino").setOrigin(0,0).setScale(0.17,0.25)
+      //this.add.image(200,300, "tile").setOrigin(0,0);
+        this.ground = this.physics.add.group();
+        this.addBase();
+    }
+    update() {
+        console.log("Je cours !")
+    }
+    addBase() {
+        let tileNeeded = Math.ceil(this.sys.game.config.width / this.tileWidth);
+        let y = this.sys.game.config.height - this.tileHeight;
+        for (let i=0; i< tileNeeded; i++) {
+            this.addTile(i * this.tileWidth, y)
+        }
+        //Rapidité : 
+        this.ground.children.iterate((child) => {
+            child.setVelocityW(Math.max(--this.groundSpeed, -230));
+        })
+
+    }
+    addTile(x,y) {
+        this.ground.create(x,y,"tile").setOrigin(0,0);
     }
 }
 let config = {
@@ -12,6 +38,15 @@ let config = {
     height:500,
     backgroundColor : '#850606', // '#4488AA',
     type:Phaser.AUTO,
+    physics : {
+        default:"arcade",
+        arcade:{
+            gravity:{
+                y:0
+            },
+            debug:true
+        }
+    }
 }
 const game = new Phaser.Game(config);
 game.scene.add("game", GameScene);
