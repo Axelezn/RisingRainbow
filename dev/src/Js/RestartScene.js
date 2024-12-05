12db40629e0203816c2d3e2eef895ad75ef5fa17
cmames <c.mames@free.fr>
2024-12-05 10:37:31 +0100

    reorg class

---
+++ dev/src/Js/RestartScene.js
@@ -0,0 +1,44 @@
+class RestartScene extends Phaser.Scene {
+    preload() {
+        this.tileWidth = 64;
+        this.tileHeight = 64;
+    }
+    create() {
+        // alert("Hello from Title");
+        const Title = this.add.text(400, 200, "RESTART GAME", {
+            fontSize: 45,
+            fontFamily: "Arial Black",
+            stroke: "gray",
+            strokeThickness: 5
+        })
+        Title.setOrigin(0.5, 0.5);
+        const spaceText = this.add.text(400, 250, "PRESS SPACE TO RESTART", {
+            fontSize: 22,
+            fontFamily: "Arial Black",
+            stroke: "gray",
+            strokeThickness: 5
+        })
+        spaceText.setOrigin(0.5, 0.5)
+        this.ground = this.physics.add.group();
+        this.addBase(0);
+
+        this.input.keyboard.once("keydown-SPACE", () => {
+            this.scene.start("game");
+        })
+        // console.log("hello from title");
+    }
+
+    addBase(x) {
+        let tileNeeded = Math.ceil((this.sys.game.config.width - x) / this.tileWidth);
+        let y = this.sys.game.config.height - this.tileHeight;
+        for (let i = 0; i < tileNeeded; i++) {
+            this.addTile(x - 10 + (i * this.tileWidth), y);
+        }
+        // velocity
+    }
+    addTile(x, y) {
+        this.ground.create(x, y, "tile").setOrigin(0, 0);
+    }
+}
+
+export default RestartScene;
