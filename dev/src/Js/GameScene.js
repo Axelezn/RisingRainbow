226215881eea0b5fbc40caa38900955cdf8e4934
Axel Ezn <axeleouzan@gmail.com>
2024-12-09 15:03:46 +0100

    Rebuild JS File, et créer le OLD

--- dev/src/Js/GameScene.js
+++ dev/src/Js/GameScene.js
@@ -19,9 +19,6 @@ class GameScene extends Phaser.Scene {
             "nuage5":"assets/parallaxes/Sol/Sol_demon/1x/Nuage_5.png",
         }
         this.load.image("plant", "assets/cactus.png");
-        this.load.atlas("dino", "assets/Démon.png", "assets/atlas/sprite.json");
-        this.load.spritesheet("bird", "assets/bird.png", { frameWidth: 150, frameHeight: 108 });
-        this.load.audioSprite("sfx", "assets/fx_mixdown.json", ["assets/fx_mixdown.mp3", "assets/fx_mixdown.ogg"])
         
         //load all images
         for (const id in images) {
@@ -41,19 +38,8 @@ class GameScene extends Phaser.Scene {
     }
     // create game entities
     create() {
-        // dino
-        this.dino = this.physics.add.sprite(200, 343, "dino").setOrigin(0, 0).setScale(0.17, 0.25);
-        // console.log(this.dino.width, this.dino.height);
-        this.dino.setSize(this.dino.width * 0.6, this.dino.height * 0.8, false).setOffset(100, 50);
-        this.dino.setGravityY(850);
-        // this.add.image(200,300,"tile").setOrigin(0,0);
-        // ground
         this.ground = this.physics.add.group();
-        // this.bird = this.physics.add.sprite(350, 250, "bird");
-        this.animation();
-        // this.bird.anims.play("fly");
-        // this.bird.setVelocityX(this.birdSpeed);
-        // scoring
+
         this.scoreText = this.add.text(600, 25, "SCORE:0", {
             fontSize: "28px",
             fontFamily: "Arial Black",
@@ -70,16 +56,7 @@ class GameScene extends Phaser.Scene {
         this.handleScore();
         // creates ground
         this.addBase(0);
-        // this.spawnBird();
-        // this.spawnPlant();
-        // this.dino.anims.play("idle");
-        // this.dino.setCollideWorldBounds(true);
-        this.physics.add.collider(this.dino, this.ground);
-        this.physics.add.collider(this.dino, this.birds, this.gameOver, null, this);
-        this.physics.add.collider(this.dino, this.plants, this.gameOver, null, this);
-        this.physics.add.collider(this.plants, this.ground);
         this.cursors = this.input.keyboard.createCursorKeys();
-        // console.log(this.cursors);
 
         // Parallaxe
         this.nuage1 = this.game.add.tileSprite(0,
@@ -116,102 +93,9 @@ class GameScene extends Phaser.Scene {
     gameOver() {
         console.log("Game Over");
         this.scene.pause();
-        this.sound.playAudioSprite("sfx", "shot");
+        //this.sound.playAudioSprite("sfx", "shot");
         localStorage.setItem("topScore", Math.max(localStorage.getItem("topScore"), this.score));
         this.scene.start("restart");
-    }
-    spawnBird() {
-        this.birds = this.physics.add.group();
-        this.time.addEvent({
-            delay: this.birdDelay,
-            loop: true,
-            callbackScope: this,
-            callback: () => {
-                let val = Math.random();
-                if (val > 0.5) {
-                    this.generateBird(280);
-                } else {
-                    this.generateBird(360);
-                }
-            }
-        })
-    }
-    generateBird(y) {
-        let bird = this.birds.create(Math.max(Math.random() * 900, 780), y, "bird");
-        bird.setScale(0.4).setOrigin(0, 0);
-        bird.setVelocityX(Math.max(--this.birdSpeed, -400));
-        bird.setSize(bird.width * 0.4, bird.height * 0.4);
-        bird.anims.play("fly");
-        this.time.addEvent({
-            delay: 4000,
-            repeat: 0,
-            callbackScope: this,
-            callback: () => {
-                console.log("Line 78", this.birds.children.size);
-                bird.destroy();
-                console.log("Line 80", this.birds.children.size);
-            }
-        })
-
-
-    }
-    spawnPlant() {
-        this.plants = this.physics.add.group();
-        this.time.addEvent({
-            delay: this.plantDelay,
-            loop: true,
-            callbackScope: this,
-            callback: () => {
-                let scale = Math.random();
-                if (scale <= 0.4) {
-                    // create 2 plants
-                    scale = 0.6;
-                    this.generatePlant(scale);
-                } else if (scale > 0.9) {
-                    scale = 0.9;
-                    this.generatePlant(scale);
-                } else {
-                    this.generatePlant(scale);
-                }
-            }
-        })
-    }
-    generatePlant(scale) {
-        // x,y 
-        let sWidth = this.sys.game.config.width;
-        let sHeight = this.sys.game.config.height;
-        let plantY = sHeight - this.tileHeight - 110;
-
-        let p1 = this.plants.create(sWidth, plantY, "plant").setOrigin(0, 0).setScale(scale);
-        p1.setVelocityX(Math.max(this.groundSpeed, -230))
-        p1.setGravityY(750);
-        p1.setSize(p1.width * 0.5, p1.height, true).setOffset(10, 0);
-        this.time.addEvent({
-            delay: 3500,
-            repeat: 0,
-            callbackScope: this,
-            callback: () => {
-                p1.destroy();
-            }
-        })
-        if (scale == 0.6) {
-            let p2 = this.plants.create(sWidth + 10, plantY, "plant").setOrigin(0, 0).setScale(scale);
-            p2.setVelocityX(Math.max(this.groundSpeed, -230))
-            p2.setGravityY(750);
-            p2.setSize(p2.width * 0.5, p2.height, true).setOffset(10, 0);
-            this.time.addEvent({
-                delay: 3500,
-                repeat: 0,
-                callbackScope: this,
-                callback: () => {
-                    p2.destroy();
-                }
-            })
-        }
-        // console.log("pSize: ",this.plants.children.size)
-
-
-
     }
     handleScore() {
         this.time.addEvent({
@@ -227,48 +111,8 @@ class GameScene extends Phaser.Scene {
             callbackScope: this
         })
     }
-    animation() {
-        this.anims.create({
-            key: "fly",
-            frames: this.anims.generateFrameNames('bird', { start: 0, end: 1 }),
-            frameRate: 20,
-            repeat: -1
-        })
-
-        // dino
-        this.anims.create({
-            key: "idle",
-            frames: this.anims.generateFrameNames("dino", {
-                start: 1,
-                end: 10,
-                prefix: "Idle_",
-                zeroPad: 2,
-                suffix: ".png",
-            }),
-            frameRate: 15,
-            repeat: -1
-        });
-        this.anims.create({
-            key: "run",
-            frames: this.anims
-                .generateFrameNames('dino', {
-                    start: 1, end: 8, zeroPad: 2,
-                    prefix: "Run_", suffix: ".png"
-                }),
-            frameRate: 15,
-            repeat: -1
-        });
-        this.anims.create({
-            key: "jump",
-            frames: this.anims.generateFrameNames('dino', {
-                start: 1, end: 12, zeroPad: 2, prefix: "Jump_", suffix: ".png"
-            }),
-            frameRate: 10,
-        });
-    }
     update() {
 
-        this.handleInput();
         // create endless ground
         // console.log("I ran");
         this.updateGround()
@@ -277,16 +121,6 @@ class GameScene extends Phaser.Scene {
         this.nuage3.tilePosition.x -= 0.3;
         this.nuage2.tilePosition.x -= 0.45;
         this.nuage1.tilePosition.x -= 0.60;
-    }
-    handleInput() {
-        // control
-        if (this.cursors.space.isDown && this.dino.body.touching.down) {
-            this.dino.setVelocityY(-500);
-            this.dino.anims.play("jump");
-            this.sound.playAudioSprite("sfx", "numkey");
-        } else if (this.dino.body.touching.down) {
-            this.dino.anims.play("run", true);
-        }
     }
     updateGround() {
         let lastBlock = this.ground.getLast(true);
