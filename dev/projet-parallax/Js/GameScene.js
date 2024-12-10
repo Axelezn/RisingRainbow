aec708f7375a6c3a5510cd795b1f717322c952a2
cmames <c.mames@free.fr>
2024-12-10 13:12:28 +0100

    scale and alpha

--- dev/projet-parallax/Js/GameScene.js
+++ dev/projet-parallax/Js/GameScene.js
@@ -21,9 +21,9 @@ class GameScene extends Phaser.Scene {
   // create game entities
   create() {
     const mult=100;
-    this.nuage5 = this.add.tileSprite(0, this.game.config.height-787/2, this.game.config.width*mult, 787, "nuage5");
-    this.nuage3 = this.add.tileSprite(0, this.game.config.height-529/2, this.game.config.width*mult, 529, "nuage3");
-    this.nuage2 = this.add.tileSprite(0, this.game.config.height-280/2, this.game.config.width*mult, 280, "nuage2");
+    this.nuage5 = this.add.tileSprite(0, this.game.config.height-787/2, this.game.config.width*mult, 787, "nuage5").setScale(0.5).setAlpha(0.8);
+    this.nuage3 = this.add.tileSprite(0, this.game.config.height-529/2, this.game.config.width*mult, 529, "nuage3").setScale(0.5).setAlpha(0.8);
+    this.nuage2 = this.add.tileSprite(0, this.game.config.height-280/2, this.game.config.width*mult, 280, "nuage2").setScale(0.5).setAlpha(0.8);
     // this.nuage2.setOrigin(0,0);
     // this.nuage3.setOrigin(0,0);
     // this.nuage5.setOrigin(0,0);
