6be60d6c7e19ef9b896e51fc1e0b37c081dcf96d
cmames <c.mames@free.fr>
2024-12-10 14:23:26 +0100

    parallax and ground

--- dev/projet-parallax/Js/GameScene.js
+++ dev/projet-parallax/Js/GameScene.js
@@ -21,19 +21,25 @@ class GameScene extends Phaser.Scene {
   // create game entities
   create() {
     const mult=100;
-    this.nuage5 = this.add.tileSprite(0, this.game.config.height-787/2, this.game.config.width*mult, 787, "nuage5").setScale(0.5).setAlpha(0.8);
-    this.nuage3 = this.add.tileSprite(0, this.game.config.height-529/2, this.game.config.width*mult, 529, "nuage3").setScale(0.5).setAlpha(0.8);
-    this.nuage2 = this.add.tileSprite(0, this.game.config.height-280/2, this.game.config.width*mult, 280, "nuage2").setScale(0.5).setAlpha(0.8);
+    this.nuage5 = this.add.tileSprite(0, this.game.config.height-500, this.game.config.width*mult, 787, "nuage5").setScale(0.5).setAlpha(0.8);
+    this.nuage4 = this.add.tileSprite(0, this.game.config.height-350, this.game.config.width*mult, 529, "nuage4").setScale(0.5).setAlpha(0.8);
+    this.nuage3 = this.add.tileSprite(0, this.game.config.height-225, this.game.config.width*mult, 533, "nuage3").setScale(0.5).setAlpha(0.8);
+    this.nuage2 = this.add.tileSprite(0, this.game.config.height-140, this.game.config.width*mult, 280, "nuage2").setScale(0.5).setAlpha(0.8);
     // this.nuage2.setOrigin(0,0);
     // this.nuage3.setOrigin(0,0);
     // this.nuage5.setOrigin(0,0);
-    this.nuage2.setScrollFactor(1);
-    this.nuage3.setScrollFactor(0.33);
-    this.nuage5.setScrollFactor(0.11);
-    this.player = this.physics.add.sprite(100, 450, 'dude');
-    this.cameras.main.startFollow(this.player);
+    this.nuage2.setScrollFactor(0.667);
+    this.nuage3.setScrollFactor(0.444);
+    this.nuage4.setScrollFactor(0.296);
+    this.nuage5.setScrollFactor(0.197);
+    this.player = this.physics.add.sprite(100, 450, 'dude'); 
+    this.cameras.main.startFollow(this.player,false,1,0.05,-300,150);
+    this.nuage1 = this.add.tileSprite(0, this.game.config.height+50, this.game.config.width*mult, 311, 'nuage1');
+    this.player.setBounce(0.2);
+    this.physics.add.existing(this.nuage1, true);
+    this.physics.add.collider(this.player, this.nuage1);
   }
-  *
+  
   gameOver() {
     console.log("Game Over");
     this.scene.pause();
@@ -46,31 +52,8 @@ class GameScene extends Phaser.Scene {
 
   update() {
     this.player.setVelocityX(160);
-
-    // this.nuage5.tilePositionX -= 0.05;
-    // this.nuage4.tilePositionX -= 0.15;
-    // this.nuage3.tilePositionX -= 0.3;
-    // this.nuage2.tilePositionX -= 0.45;
-    // this.nuage1.tilePositionX -= 0.6;
   }
 
-  addBase(x) {
-    let tileNeeded = Math.ceil(
-      (this.sys.game.config.width - x) / this.tileWidth
-    );
-    let y = this.sys.game.config.height - this.tileHeight;
-    for (let i = 0; i < tileNeeded; i++) {
-      this.addTile(x - 10 + i * this.tileWidth, y);
-    }
-    // velocity
-    this.ground.children.iterate((child) => {
-      child.setVelocityX(Math.max(--this.groundSpeed, -230));
-      child.setImmovable(true);
-    });
-  }
-  addTile(x, y) {
-    this.ground.create(x, y, "nuage1").setOrigin(0, 0);
-  }
 }
 
 export default GameScene;
